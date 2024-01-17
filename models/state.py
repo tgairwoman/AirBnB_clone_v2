0d3910e35ce6d2879a31233767e9e69c71fff8a4
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-17 14:17:16 +0300

    many to many with back populate

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete, delete-orphan",
-                          backref="state")
+                              backref="state")
 
     @property
     def cities(self):
