96dc9790816f9fbbdd2e62fc355f3e1591c218c6
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-17 13:28:18 +0300

    many to many

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from os import getenv
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
@@ -25,35 +26,36 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship('Review', cascade='all, delete, delete-orphan',
-                           backref='place')
-    amenities = relationship('Amenity', secondary=place_amenity,
-                             viewonly=False)
     amenity_ids = []
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', cascade='all, delete, delete-orphan',
+                               backref='place')
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 viewonly=False, back_populates="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """returns the list of Review instances with
+            place_id equals to the current place.id
+            """
+            from models import storage
+            from models.review import Review
 
-    @property
-    def reviews(self):
-        """returns the list of Review instances with
-        place_id equals to the current place.id
-        """
-        from models import storage
-        from models.review import Review
+            _all = storage.all(Review)
+            result = []
+            for instance in _all.values():
+                if instance.place_id == self.id:
+                    result.append(instance)
+            return result
 
-        _all = storage.all(Review)
-        result = []
-        for instance in _all.values():
-            if instance.place_id == self.id:
-                result.append(instance)
-        return result
+        @property
+        def amenities(self):
+            """returns a list of Amenity instances based on the amenity_ids"""
+            return self.amenity_ids
 
-    @property
-    def amenities(self):
-        """returns a list of Amenity instances based on the amenity_ids"""
-        return self.amenity_ids
-
-    @amenities.setter
-    def amenities(self, obj):
-        """a setter method for amenity_ids"""
-        from models.amenity import Amenity
-        if type(obj) is Amenity and obj.id not in self.amenity_ids:
-            self.amenity_ids.append(obj.id)
+        @amenities.setter
+        def amenities(self, obj):
+            """a setter method for amenity_ids"""
+            from models.amenity import Amenity
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
