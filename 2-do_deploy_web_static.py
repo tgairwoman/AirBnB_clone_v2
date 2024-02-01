bea1e72222add37f19d2217e59192036a8141e3a
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-01 01:57:30 -0800

    remove the file path check

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import os
 
 
-env.hosts = ['ubuntu@54.198.80.67', 'ubuntu@100.26.152.235']
+env.hosts = ['54.198.80.67', '100.26.152.235']
 
 def do_pack():
     """pack a web_static folder to .tgz"""
@@ -20,21 +20,19 @@ def do_pack():
 
 def do_deploy(archive_path):
     """deploys web_static to web servers"""
-    if os.path.exists(archive_path):
-        try:
-            _name_w_extension = archive_path.split('/')[-1]
-            _name = archive_path.split('/')[-1].split('.')[0]
-            put(archive_path, "/tmp/")
-            run("mkdir -p /data/web_static/releases/{}/".format(_name))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(_name_w_extension, _name))
-            run("rm {}".format(_name_w_extension))
-            run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(_name, _name))
-            run("rm -rf /data/web_static/releases/{}/web_static".format(_name))
-            run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{}/ /data/web_static_current".format(_name))
-            print("New version deployed!")
-            return True
-        except Exception:
-            return False
-    else:
+    try:
+        _name_w_extension = archive_path.split('/')[-1]
+        _name = archive_path.split('/')[-1].split('.')[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(_name_w_extension, _name))
+        run("rm /tmp/{}".format(_name_w_extension))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(_name, _name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(_name))
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        print(e)
         return False
